0475293e416cfd88f1a5099668cc5569f4935a91
TayfunKatilmis <60717672+Tayfun08@users.noreply.github.com>
2024-02-16 22:40:06 +0300

    db and requirements

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo_app',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,9 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "todoDb",
+        "USER": "postgres",
     }
 }
 
