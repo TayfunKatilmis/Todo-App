16392f12025544421a3f55fdae3277ec11204201
TayfunKatilmis <60717672+Tayfun08@users.noreply.github.com>
2024-02-17 16:39:16 +0300

    added delete views

--- todo_project/todo_app/urls.py
+++ todo_project/todo_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import TodoList,TodoDetail,TodoCreate
+from .views import TodoList,TodoDetail,TodoCreate, TodoUpdate,TodoDelete
 
 urlpatterns = [
     path("", TodoList.as_view(), name="todoItems"),
     path("todo/<int:pk>/", TodoDetail.as_view(), name="todoItems"),
     path("todo-create/", TodoCreate.as_view(), name="todoItems-create"),
+    path("todo-update/<int:pk>/", TodoUpdate.as_view(), name="todoItems-update"),
+    path("todo-delete/<int:pk>/", TodoDelete.as_view(), name="todoItems-delete"),
 ]
--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import todoItem
 # Create your views here.
@@ -22,3 +22,16 @@ class TodoCreate(CreateView):
     fields = '__all__'
     success_url = reverse_lazy('todoItems')
     template_name = 'todo_app/todo_form.html'
+
+class TodoUpdate(UpdateView):
+    model = todoItem
+    fields = '__all__'
+    success_url = reverse_lazy('todoItems')
+    template_name = 'todo_app/todo_form.html'
+
+
+class TodoDelete(DeleteView):
+    model = todoItem
+    context_object_name = 'todo'
+    success_url = reverse_lazy('todoItems')
+    template_name = 'todo_app/todo_confirm_delete.html'
